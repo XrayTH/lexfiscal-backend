80ad74137f0ecf5c241596905bc76e4fd2c83bf2
XrayTH <143976649+XrayTH@users.noreply.github.com>
2026-01-31 19:44:46 -0500

    fix: cors fix

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv"
 import express from "express"
 import helmet from "helmet"
 import rateLimiter from "./middlewares/rateLimit.js"
@@ -5,12 +6,18 @@ import healthRoutes from "./routes/health.routes.js"
 import tributeRoutes from "./routes/tribute.routes.js"
 import getFramework from "./routes/framework.routes.js"
 import errorHandler from './middlewares/errorHandler.js';
+import cors from "cors"
 
 const app=express()
+dotenv.config()
 
 app.use(helmet())
 app.use(errorHandler)
 app.use(express.json())
+app.use(cors({
+ origin:process.env.FRONTEND_URL,
+ methods:["GET"]
+}))
 
 if(process.env.RATE_LIMIT_ENABLED==="true"){
   app.use(rateLimiter)
